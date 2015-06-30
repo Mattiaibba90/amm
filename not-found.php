4fc3c6e6270b82e9046f11f066b0cbd95b697fb6
Mattiaibba90 <mattiaibba90@hotmail.it>
2015-06-30 20:47:04 +0200

    Create not-found.php

---
+++ not-found.php
@@ -0,0 +1,27 @@
+<?php
+include_once 'Settings.php';
+?>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <base href="<?= Settings::getApplicationPath() ?>"/>
+        <title>Errore</title>
+    </head>
+    
+    <body>
+        <h1>Siamo spiacenti, si &egrave; verificato un errore:</h1>
+        <br/>
+        <h2><?= $titolo ?></h2>
+        <p>
+            <?=
+            $messaggio
+            ?>
+        </p>
+        <?php if (isset($canLogin)) { ?>
+            <p>Puoi autenticarti nella seguente pagina di <a href="login">login</a></p>
+        <?php    } ?>
+    </body>
+</html>
