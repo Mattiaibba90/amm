61e2c58d18c47c2ed84e1f6805d28ea509f28b84
Mattiaibba90 <mattiaibba90@hotmail.it>
2015-06-30 20:48:10 +0200

    Create Settings.php

---
+++ Settings.php
@@ -0,0 +1,39 @@
+<?php
+
+// Classe con le impostazioni per il database
+
+class Settings {
+
+    const BIJOUX_HOME = 6;
+    
+    private static $appPath;
+    public static $db_host = 'localhost';
+    public static $db_user = 'mattiaibba90';
+    public static $db_password = 'mamaluigi1?';
+    public static $db_name = 'mattiaibba90';
+
+    // gestisco il path per l'applicazione, che sia in locale o sul server pubblico
+    
+    public static function getApplicationPath() {
+        if (!isset(self::$appPath)) {
+            // restituisce il server corrente
+            switch ($_SERVER['HTTP_HOST']) {
+                case 'localhost':
+                    // configurazione locale
+                    self::$appPath = 'http://' . $_SERVER['HTTP_HOST'] . '/PhpProjectAmm';
+                    break;
+                case 'spano.sc.unica.it':
+                    // configurazione pubblica
+                    self::$appPath = 'http://' . $_SERVER['HTTP_HOST'] . '/amm2015/ibbaMattia';
+                    break;
+
+                default:
+                    self::$appPath = '';
+                    break;
+            }
+        }
+        
+        return self::$appPath;
+    }
+
+}
