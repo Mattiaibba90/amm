0f5d058bb666abb654dc85405c87b4f1b9adc1e7
Mattiaibba90 <mattiaibba90@hotmail.it>
2015-07-17 19:42:55 +0200

    Delete login_content.php

--- view/login/login_content.php
+++
@@ -1,4 +0,0 @@
-<input type="text" id="Username" name="Username" maxlength="20" value="Username" style="text-align: center"> <br>
-<input type="password" id="Password" name="Password" maxlength="16" value="Password" style="text-align: center"> <br>
-<input type="submit" name="Login" value="Login"> <br>
-<input type="hidden" name="IE" value="IE">
