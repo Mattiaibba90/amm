506edfd1cbc1b52a36c09feb5ff3ecacb4ef4d6c
Mattiaibba90 <mattiaibba90@hotmail.it>
2015-07-19 13:40:36 +0200

    Update register.php

--- view/login/register.php
+++ view/login/register.php
@@ -1,1 +1,75 @@
-
+<span id="confirm_registration" class="form_confirm"> </span>
+<ul>
+    <li>
+        <h3>Registrati qui!</h3>
+        <p>Compila tutti i campi:</p>
+                    <ul class="form_ul">
+                        <li>
+                            <label for="username">Username:</label>
+                            <input type="text" required="required" name="username" id="username">
+                            <span id="error_username" class="form_error"> </span>
+                        </li>
+                        <li>
+                            <label for="password">Password:</label>
+                            <input type="text" required="required" name="password" id="password">
+                            <span id="error_password" class="form_error"> </span>
+                        </li>
+                        <li>
+                            <label for="name">Nome:</label>
+                            <input type="text" required="required" name="name" id="name">
+                            <span id="error_name" class="form_error"> </span>
+                        </li>
+                        <li>
+                            <label for="surname">Cognome:</label>
+                            <input type="text" required="required" name="surname" id="surname">
+                            <span id="error_surname" class="form_error"> </span>
+                        </li>
+                        <li>
+                            <label for="mail">e-mail:</label>
+                            <input type="text" required="required" name="mail" id="mail">
+                            <span id="error_mail" class="form_error"> </span>
+                        </li>
+                        <li>
+                            <label for="creditCard">Circuito della carta di credito:</label>
+                            <input type="text" required="required" name="creditCard" id="creditCard">
+                            <span id="error_creditCard" class="form_error"> </span>
+                        </li>
+                        <li>
+                            <label for="creditCardNumber">Numero della carta di credito:</label>
+                            <input type="text" required="required" name="creditCardNumber" id="creditCardNumber">
+                            <span id="error_creditCardNumber" class="form_error"> </span>
+                        </li>
+                    </ul>
+                    <br/>
+                </li>
+                <li>
+                    Indirizzo di fatturazione:
+                    <br/>
+                    <ul class="form_ul">
+                        <li>
+                            <label for="city">Citt&agrave; :</label>
+                            <input type="text" required="required" name="city" id="city">
+                            <span id="error_city" class="form_error"> </span>
+                        </li>
+                        <li>
+                            <label for="cap">CAP :</label>
+                            <input type="text" required="required" name="cap" id="cap">
+                            <span id="error_cap" class="form_error"> </span>
+                        </li>
+                        <li>
+                            <label for="street">Via o Piazza:</label>
+                            <input type="text" required="required" name="street" id="street">
+                            <span id="error_street" class="form_error"> </span>
+                        </li>
+                        <li>
+                            <label for="streetNumber">Numero Civico:</label>
+                            <input type="text" required="required" name="streetNumber" id="streetNumber">
+                            <span id="error_streetNumber" class="form_error"> </span>
+                        </li>
+                    </ul>
+                    <br/>
+                </li>
+            </ul>
+        <p><button id="submit-registration">Conferma Registrazione</button></p>
+    </li>
+</ul>
