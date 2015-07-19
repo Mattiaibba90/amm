94bc5f969e44e0116e8dedcf07efce8c25248414
Mattiaibba90 <mattiaibba90@hotmail.it>
2015-07-19 22:57:07 +0200

    Update content.php

--- view/admin/content.php
+++ view/admin/content.php
@@ -1,1 +1,24 @@
-
+<ul>
+        <li>
+            <h3>Pannello Amministratore</h3>
+            <ul>
+                <li>
+                    <a href="admin/amministraUtenti?">Amministra utenti</a>
+                </li>
+                <li>
+                    <a href="admin/ricaricaUtenti?">Ricarica utenti</a>
+                </li>
+                <li>
+                    <a href="admin/amministraBijoux?">Amministra bijoux</a>
+                </li>
+                <li>
+                    <a href="admin/home?">Mostra home</a>
+                </li>
+                <li>
+                    <form method="post" action="admin/logout">
+                        <button type="submit" name="cmd" value="logout">Logout</button>
+                    </form>
+                </li>
+            </ul>
+        </li>
+    </ul>
