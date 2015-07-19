adcbf57b9accf1d1ccb3a8458aff25e08f64b95b
Mattiaibba90 <mattiaibba90@hotmail.it>
2015-07-19 19:28:47 +0200

    Create content.php

---
+++ view/utente/content.php
@@ -0,0 +1,18 @@
+<?php
+if($pageContent->getSubPage()=='register')
+{
+        include 'register.php';
+}
+else
+{
+?>
+<h4>Sei tornato? Accedi qui</h4><br><br>
+
+<input type="text" id="Username" name="Username" maxlength="20" value="Username" style="text-align: center"> <br>
+<input type="password" id="Password" name="Password" maxlength="16" value="Password" style="text-align: center"> <br>
+<input type="submit" name="Login" value="Login"> <br>
+<input type="hidden" name="IE" value="IE">
+
+<h4>Altrimenti inserisci i dati e registrati <a href=ibbaMattia/view/login/register.php>QUI</a>, è facile!</h4>
+<?php
+}
