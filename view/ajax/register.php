4fe387b62d67bcca6b0d0dd8df3d6b8f46bd6971
Mattiaibba90 <mattiaibba90@hotmail.it>
2015-07-19 15:45:08 +0200

    Create register.php

---
+++ view/ajax/register.php
@@ -0,0 +1,41 @@
+<?php
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+
+$json = array();
+
+if(isset($risposta['username']))
+    $json["username"] = $risposta['username']->getMessage();
+
+if(isset($risposta['password']))
+    $json["password"] = $risposta['password']->getMessage();
+
+if(isset($risposta['name']))
+    $json["name"] = $risposta['name']->getMessage();
+
+if(isset($risposta['surname']))
+    $json["surname"] = $risposta['surname']->getMessage();
+
+if(isset($risposta['mail']))
+    $json["mail"] = $risposta['mail']->getMessage();
+
+if(isset($risposta['creditCard']))
+    $json["creditCard"] = $risposta['creditCard']->getMessage();
+
+if(isset($risposta['creditCardNumber']))
+    $json["creditCardNumber"] = $risposta['creditCardNumber']->getMessage();
+
+if(isset($risposta['city']))
+    $json["city"] = $risposta['city']->getMessage();
+
+if(isset($risposta['cap']))
+    $json["cap"] = $risposta['cap']->getMessage();
+
+if(isset($risposta['street']))
+    $json["street"] = $risposta['street']->getMessage();
+
+if(isset($risposta['streetNumber']))
+    $json["streetNumber"] = $risposta['streetNumber']->getMessage();
+
+echo json_encode($json);
+?>
