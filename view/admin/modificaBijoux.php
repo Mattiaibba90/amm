18e142a8b9c86a8108cd0dd0bc5822ee97a730b0
Mattiaibba90 <mattiaibba90@hotmail.it>
2015-07-19 23:57:39 +0200

    Create modificaBijoux.php

---
+++ view/admin/modificaBijoux.php
@@ -0,0 +1,34 @@
+<h3>Modifica Bijou</h3>
+    <p>Compila il form:</p>
+            <form action="admin/home" method="post">
+            <ul class="form_ul">
+                <li>
+                    <label for="name">Nome:</label>
+                    <input type="text" required="required" name="name" id="name">
+                </li>
+                <li>
+                    <label for="material">Materiale:</label>
+                    <input type="text" required="required" name="material" id="material">
+                </li>
+                <li>
+                    <p>
+                        Tipo di bijou:
+                        <select name="typeBijou" required="required">
+                            <option></option>
+                            <option value="Amigurumi">Amigurumi</option>
+                            <option value="Gioielli">Gioielli</option>
+                            <option value="Parure">Parure</option>
+                        </select>
+                    </p>
+                </li>
+                <li>
+                    <label for="avaibility">Disponibilità:</label>
+                    <input type="text" required="required" name="avaibility" id="avaibility">
+                </li>
+                <li>
+                    <label for="actualPrice">Prezzo:</label>
+                    <input type="text" required="required" name="actualPrice" id="actualPrice">
+                </li>
+            </ul>
+        <p><button type="submit" name="cmd" value="vendiBijou">Conferma</button></p>
+    </form>
