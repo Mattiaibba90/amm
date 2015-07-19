ca3934e08ba9f8dbf167cfdf4e8f63789129ea85
Mattiaibba90 <mattiaibba90@hotmail.it>
2015-07-19 22:48:33 +0200

    Update sidebar.php

--- view/admin/sidebar.php
+++ view/admin/sidebar.php
@@ -1,1 +1,8 @@
+<ul>                 
+    <li><h2 class="icon-title">Navigazione</h2></li>
+    <li><a href="#">Home</a></li>
+    <li><a href="Bijougioielli.html">Bijou</a></li>
+    <li><a href="Bijouamigurumi.html">Amigurumi</a></li>
+    <li><a href="Bijoucontatti.html">Contatti</a></li>
+</ul>
 
