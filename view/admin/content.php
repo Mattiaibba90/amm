e1b5e557fcd814b52884f8aabc40b84d83fada45
Mattiaibba90 <mattiaibba90@hotmail.it>
2015-07-20 20:09:29 +0200

    Update content.php

--- view/admin/content.php
+++ view/admin/content.php
@@ -1,24 +1,45 @@
-<ul>
-        <li>
-            <h3>Pannello Amministratore</h3>
+<?php
+switch ($pageContent->getSubPage()) {
+    
+    case 'amministraBijoux':
+        include 'amministraBijoux.php';
+        break;
+    
+    case 'risultatiRicercaAvanzata':
+        include 'risultatiRicercaAvanzata.php';
+        break;
+
+    case 'amministraUtenti':
+        include 'amministraUtenti.php';
+        break;
+    
+    case 'modificaBijoux':
+        include 'modificaBijoux.php';
+        break;
+    
+    case 'modificaUtenti':
+        include 'modificaUtenti.php';
+        break;
+    
+    case 'ricaricaUtenti':
+        include 'ricaricaUtenti.php';
+        break;    
+    
+    case 'pannelloControllo':
+        include 'pannelloControllo.php';
+        break;
+
+    default: ?>
             <ul>
-                <li>
-                    <a href="admin/amministraUtenti?">Amministra utenti</a>
-                </li>
-                <li>
-                    <a href="admin/ricaricaUtenti?">Ricarica utenti</a>
-                </li>
-                <li>
-                    <a href="admin/amministraBijoux?">Amministra bijoux</a>
-                </li>
-                <li>
-                    <a href="admin/home?">Mostra home</a>
-                </li>
-                <li>
-                    <form method="post" action="admin/logout">
-                        <button type="submit" name="cmd" value="logout">Logout</button>
-                    </form>
+                <li><h3>Ultimi arrivi</h3>
+                    <ul class="lista_visiva">
+                        <?php 
+                            foreach ($ultimiArrivi as $ultimoArrivo) {
+                                echo '<li><a href="utente/bijou?id_bijou=' . $ultimoArrivo->getId() . '">' . $ultimoArrivo->getNameBijou() . '</a></li>';
+                            }
+                            ?>
+                    </ul>
                 </li>
             </ul>
-        </li>
-    </ul>
+<?php break;}
+                
