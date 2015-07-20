99410c2f145a9fe7342c9ad37540c350061a6a39
Mattiaibba90 <mattiaibba90@hotmail.it>
2015-07-20 20:05:11 +0200

    Update sidebar.php

--- view/login/sidebar.php
+++ view/login/sidebar.php
@@ -1,7 +1,7 @@
 <ul>                 
     <li><h2 class="icon-title">Navigazione</h2></li>
-    <li><a href="#">Home</a></li>
-    <li><a href="Bijougioielli.html">Bijou</a></li>
-    <li><a href="Bijouamigurumi.html">Amigurumi</a></li>
-    <li><a href="Bijoucontatti.html">Contatti</a></li>
+    <li><a href="login/home">Home</a></li>
+    <li><a href="login/risultatiRicercaAvanzata?cmd=ricerca_avanzata&amp;typeBijou=Gioiello">Bijou</a></li>
+    <li><a href="login/risultatiRicercaAvanzata?cmd=ricerca_avanzata&amp;typeBijou=Amigurumi">Amigurumi</a></li>
+    <li><a href="login/risultatiRicercaAvanzata?cmd=ricerca_avanzata&amp;typeBijou=Parure">Parure</a></li>
 </ul>
