49892f8d39528e0424851e507acf1f24f99e31dc
Mattiaibba90 <mattiaibba90@hotmail.it>
2015-07-22 22:18:32 +0200

    Create mostraBijou.php

---
+++ view/admin/mostraBijou.php
@@ -0,0 +1,9 @@
+<ul>
+    <li>
+        <h3><?= $bijou->getNameBijou(); ?></h3>
+    </li>
+    <li><p>Materiale: <?= $bijou->getMaterial(); ?></p></li>
+    <li><p>Categoria: <?= $bijou->getTypeBijou(); ?></p></li>
+    <li><p>Disponibilit&agrave;: <?= $bijou->getAvaibility(); ?></p></li>
+    <li><p>Prezzo: <?= $bijou->getActualPrice(); ?></p></li>
+</ul>
