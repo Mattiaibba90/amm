57e2bc9662fdf62bcdccda22a09a862a1cb267b5
Mattiaibba90 <mattiaibba90@hotmail.it>
2015-09-22 18:27:05 +0200

    Update Accounts.php

--- model/Accounts.php
+++ model/Accounts.php
@@ -39,7 +39,6 @@ class Accounts {
                     $return->setId($id);
                 } //se è un utente registrato                    
                 else{
-                    //$stmt = $mysqli->stmt_init();
                     $query = "SELECT * FROM admin WHERE username_admin=? AND password_admin=?";
                     $stmt->prepare($query);
                     $stmt->bind_param("ss", $username, $password);
